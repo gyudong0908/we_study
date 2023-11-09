0db4a7413a8d5a430803fdcc92d695cc2ebcf7f8
gyudong <dongco213@naver.com>
2023-11-09 15:02:52 +0900

    feat 클래스 생성 기능

--- client/src/components/MyModal/CreateClassModal.jsx
+++ client/src/components/MyModal/CreateClassModal.jsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import {Stack, Typography, Modal, Button, TextField} from '@mui/material';
+import { useState } from 'react';
+import axios from 'axios';
 
 const style = {
   position: 'absolute',
@@ -14,6 +16,25 @@ const style = {
 };
 
 function CreateClassModal({open, handleClose}) {
+   const [name,setName] = useState('');
+   const [teacher,setTeacher] = useState('');
+   const [description,setDescription] = useState('');
+
+  function createClass(){      
+    if(name !=='' && teacher !==''){
+      const data = {
+        name : name,
+        section	: teacher,
+        description : description,
+        ownerId : "me"
+      }
+      // axios.post('http://localhost:8081/class',data,{ withCredentials: true }).then(()=>{
+      //   alert('생성이 완료 되었습니다.');
+      // }).catch(err=>{
+      //   alert('오류발생',err);
+      // })
+    }
+  }
 
   return (
     <div>
@@ -36,6 +57,8 @@ function CreateClassModal({open, handleClose}) {
                 rows={8}
                 sx={{ mt: 2 }}
                 required
+                onChange={(e)=>{setName(e.target.value);}}
+                value={name}
                 />
             <TextField
                 id="outlined-basic"
@@ -45,6 +68,8 @@ function CreateClassModal({open, handleClose}) {
                 rows={8}
                 sx={{ mt: 2 }}
                 required
+                onChange={(e)=>{setTeacher(e.target.value);}}
+                value={teacher}
                 />
             <TextField
                 id="outlined-basic"
@@ -53,11 +78,13 @@ function CreateClassModal({open, handleClose}) {
                 fullWidth
                 rows={8}
                 sx={{ mt: 2 }}
+                onChange={(e)=>{setDescription(e.target.value);}}
+                value={description}
                 />
           </Stack>
           <Stack direction="row" justifyContent="flex-end" gap={1} sx={{mt:2}}>
             <Button variant="outlined" type="reset" onClick={handleClose}>취소</Button>
-            <Button variant="outlined">생성</Button>
+            <Button variant="outlined" onClick={()=>{createClass();handleClose();}}>생성</Button>
         </Stack>
         </Stack>
       </Modal>
