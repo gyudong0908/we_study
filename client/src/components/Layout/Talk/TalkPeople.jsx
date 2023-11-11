3c0c7e9af4b420902a57a6d5c9370b23ea4c5122
donggyu <dongco213@naver.com>
2023-11-11 17:35:45 +0900

    fix 채팅방 ui 수정

--- client/src/components/Layout/Talk/TalkPeople.jsx
+++ client/src/components/Layout/Talk/TalkPeople.jsx
@@ -1,25 +1,31 @@
-import { Grid, Stack } from "@mui/material";
-import {Typography} from "@mui/material";
+import { Grid, Box, styled } from "@mui/material";
+import { Typography } from "@mui/material";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import CircleRoundedIcon from '@mui/icons-material/CircleRounded';
 
-export default function TalkPeople(props){
-    return(
-        <Stack sx={{cursor: 'pointer'}}>
-        {props.data.map(data=>{
-            return(
-                <Grid container>
-                {/* 이 밑은 지금은 icon으로 넣었지만 나중에 profile사진으로 넣을 수 있음 */}
-                <Grid item>
-                <AccountCircleIcon sx={{
-                    fontSize : '40pt',
-                }}/>
-                </Grid>
-                <Grid item> 
-                    <Typography variant='h4' sx={{alignSelf : 'center', marginLeft : '20px'}}>{data.name}</Typography>
-                </Grid>
-            </Grid>
-            )
-        })}
-        </Stack>
+export default function TalkPeople(props) {
+    const nows = ['이동규'];
+    return (
+        <Box sx={{ cursor: 'pointer' }}>
+            {props.data.map(data => {
+                return (
+                    <Grid container sx={{ '&:hover': { background: 'rgba(0, 0, 0, 0.1)' } }} >
+                        {/* 이 밑은 지금은 icon으로 넣었지만 나중에 profile사진으로 넣을 수 있음 */}
+                        <Grid item sx={{ marginLeft: '10px' }} xs={1}>
+                            <AccountCircleIcon sx={{
+                                fontSize: '30pt',
+                            }} />
+                        </Grid>
+                        <Grid item xs={10} sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <Typography variant='h5' sx={{ alignSelf: 'center', marginLeft: '20px', paddingTop: '3px' }}>{data.name}</Typography>
+                            {
+                                nows.map(now => { if (now === data.name) return <CircleRoundedIcon sx={{ marginTop: '10px', color: 'red' }} /> })
+                            }
+
+                        </Grid >
+                    </Grid>
+                )
+            })}
+        </Box>
     )
 }
--- client/src/components/Layout/Talk/index.jsx
+++ client/src/components/Layout/Talk/index.jsx
@@ -1,100 +1,76 @@
 import Box from '@mui/material/Box';
-import Tab from '@mui/material/Tab';
-import Tabs from '@mui/material/Tabs';
-import PropTypes from 'prop-types';
 import * as React from 'react';
 import CloseIcon from '@mui/icons-material/Close';
 import TalkPeople from './TalkPeople';
-import { Stack } from '@mui/material';
+import { Button, Stack, styled, Typography } from '@mui/material';
 
-function CustomTabPanel(props) {
-  const { children, value, index, ...other } = props;
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
-    </div>
-  );
-}
-
-CustomTabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
-
-export default function Talk({closeTalk}) {
+export default function Talk({ closeTalk }) {
   const [value, setValue] = React.useState(0);
+
+  const TabButton = styled(Button)(({ theme, isActive }) => ({
+    margin: '10px',
+    border: '1px solid black',
+    flex: 1,
+    color: isActive ? 'white' : 'black',
+    backgroundColor: isActive ? 'grey' : 'white',
+    '&:hover': {
+      backgroundColor: isActive ? 'grey' : 'white',
+    },
+  }));
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
   const classData = [
-    {name:"KOSTA 265기"},
-    {name:"2023 OUTTA 데이터분석"},
+    { name: "KOSTA 265기" },
+    { name: "2023 OUTTA 데이터분석" },
   ];
   const personalData = [
-    {name:"이동규"},
-    {name:"조정석"},
-    {name:"최혜린"},
-  ]
+    { name: "이동규" },
+    { name: "조정석" },
+    { name: "최혜린" },
+  ];
 
 
   const tabs = [
     {
       title: '클래스',
-      content: <TalkPeople data={classData}/>,
+      content: <TalkPeople data={classData} />,
     },
     {
       title: '개인',
-      content: <TalkPeople data={personalData}/>,
+      content: <TalkPeople data={personalData} />,
     },
   ];
 
   return (
     <Box sx={{
-         width: 550,
-         height: 500,
-         boxShadow: '0 4px 8px rgba(0, 0, 0, 0.5)',
-         position:'fixed',
-         bottom: 0,
-         right: 0,
-         background : 'white',
-         display: 'flex',
-         justifyContent: 'space-between'
-        }}>
+      width: 400,
+      height: 500,
+      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.5)',
+      position: 'fixed',
+      bottom: 0,
+      right: 0,
+      background: 'white',
+    }}>
       <Stack>
-        <Box>
-            <Tabs
-            value={value}
-            onChange={handleChange}
-            aria-label="basic tabs example"
-            >
-            {tabs.map((tab, index) => (
-                <Tab key={index} label={tab.title} {...a11yProps(index)} />
-            ))}
-            </Tabs>
+        <Stack sx={{ textAlign: 'right', display: 'inline-block' }}>
+          <CloseIcon onClick={closeTalk} sx={{ cursor: 'pointer', '&:hover': { border: '2px solid skyblue' } }} />
+        </Stack>
+        <Box sx={{ display: 'flex', justifyContent: 'center', marginBottom: '10px' }}>
+          <TabButton onClick={() => { setValue(0) }} isActive={value === 0}>
+            <Typography variant='h5'>클래스</Typography>
+          </TabButton>
+          <TabButton onClick={() => { setValue(1) }} isActive={value === 1}>
+            <Typography variant='h5'>개인</Typography>
+          </TabButton>
         </Box>
-        {tabs.map((tab, index) => (
-            <CustomTabPanel key={index} value={value} index={index}>
-            {tab.content}
-            </CustomTabPanel>
-        ))}
-    </Stack>
-    <CloseIcon onClick = {closeTalk} sx={{cursor:'pointer'}}/>
+        {
+          value === 0 ? <TalkPeople data={classData} /> : <TalkPeople data={personalData} />
+        }
+      </Stack>
     </Box>
   );
 }
