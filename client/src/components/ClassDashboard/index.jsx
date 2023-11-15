5f728d93904bc16adb01a39c8858c0c77326ae37
gyudong <dongco213@naver.com>
2023-11-15 17:28:32 +0900

    feat dashboardPage 데이터 바인딩

--- client/src/components/ClassDashboard/index.jsx
+++ client/src/components/ClassDashboard/index.jsx
@@ -3,17 +3,46 @@ import StudyProgress from './StudyProgress';
 import { Stack, Typography } from '@mui/material';
 import InputCurriculum from '../Input/InputCurriculum.jsx';
 import PrivateProgress from './PrivateProgress.jsx';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-export default function ClassDashboard({ curriculums, isTeacher }) {
+export default function ClassDashboard({isTeacher }) {
+  const [curriculums,setCurriculums] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const { classId } = useParams();
+
+  async function getCurriculums(){
+    try{
+      const data =  await axios.get(`http://localhost:8081/works?classId=${classId}&category=커리큘럼`,{ withCredentials: true });
+      const sortData = data.data.sort((a,b)=> new Date(a.creationTime) - new Date(b.creationTime));
+      setCurriculums(sortData);
+      setIsLoading(true);
+    }catch{
+      alert('에러 발생');
+      console.log(err);
+    }
+  }
+
+  useEffect(()=>{
+    getCurriculums()
+    return setIsLoading(false);
+  },[])
+
   return (
-    <Stack>
-      <InputCurriculum isTeacher={isTeacher} marginBottom={20}></InputCurriculum>
-      <PrivateProgress />
-      <StudyProgress />
-      <Typography variant="h4" component="span" sx={{mb:2, fontWeight: 'bold', color:'#0091ea'}}>
-        커리큘럼
-      </Typography>
-      <BasicAccordion isTeacher={isTeacher} curriculums={curriculums} />
-    </Stack>
+    <>
+    {isLoading?
+      <Stack>
+        <InputCurriculum isTeacher={isTeacher} marginBottom={20} setCurriculums={setCurriculums}></InputCurriculum>
+        <PrivateProgress />
+        <StudyProgress />
+        <Typography variant="h4" component="span" sx={{mb:2, fontWeight: 'bold', color:'#0091ea'}}>
+          커리큘럼
+        </Typography>
+        <BasicAccordion isTeacher={isTeacher} curriculums={curriculums} setCurriculums={setCurriculums} />
+      </Stack>:
+      null
+   }
+    </>
   );
 }
