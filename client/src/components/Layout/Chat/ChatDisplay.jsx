75aacdcfdc5063fb0927246af4fb27e9495aafcc
donggyu <dongco213@naver.com>
2023-11-19 21:59:52 +0900

    fix 채팅 ui 수정

--- client/src/components/Layout/Chat/ChatDisplay.jsx
+++ client/src/components/Layout/Chat/ChatDisplay.jsx
@@ -1,35 +1,47 @@
 import io from "socket.io-client";
-import { TextField, Grid, Button, Typography, Box } from '@mui/material';
+import { TextField, Grid, Button, Typography, Box, Stack, Avatar } from '@mui/material';
 import { useRef, useEffect, useState } from "react";
 import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
+import dayjs from 'dayjs';
 
-function RightChat({ content }) {
+function RightChat({ content, createdAt }) {
     return (
-        <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <Typography sx={{ backgroundColor: 'yellow', margin: '3px', border: '1px solid black', borderRadius: '2px' }} variant='body1'>{content}</Typography>
-        </Box>
+        <Stack>
+            <Box sx={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'flex-end', marginBottom: '10px' }}>
+                <Typography variant='body2' sx={{ marginTop: '12px' }}>{dayjs(createdAt).format('h:mm:ss A')}</Typography>
+                <Typography sx={{ backgroundColor: 'yellow', margin: '3px', border: '1px solid black', borderRadius: '2px', maxWidth: 270 }} variant='body1'>{content}</Typography>
+            </Box>
+        </Stack>
     )
 }
 
-function LeftChat({ content }) {
+function LeftChat({ content, user, createdAt }) {
     return (
-        <Box sx={{ display: 'flex', justifyContent: 'flex-start' }}>
-            <Typography sx={{ backgroundColor: 'yellow', margin: '3px', border: '1px solid black', borderRadius: '2px' }} variant='body1'>{content}</Typography>
+        <Box sx={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'flex-end', marginBottom: '10px' }}>
+            <Avatar sx={{ height: 50, width: 50 }} src={user.downloadPath} />
+            <Stack>
+                <Typography>{user.nickName}</Typography>
+                <Typography sx={{ backgroundColor: 'yellow', margin: '3px', border: '1px solid black', borderRadius: '2px', maxWidth: 270 }} variant='body1'>{content}</Typography>
+            </Stack>
+            <Typography variant='body2' sx={{ marginTop: '12px' }}>{dayjs(createdAt).format('h:mm:ss A')}</Typography>
         </Box>
     )
 }
 export default function ChatDisplay({ rewind, value, classChatId, chatTitle, chatUserId, socket }) {
     const [contents, setContents] = useState([]);
     const [sendData, setSendData] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
     const user = useSelector(state => state.userData);
+    const stackRef = useRef(null);
     const state = value === 0 ? 'classChat' : 'individualChat';
 
     function getMessage() {
         axios.get(`http://localhost:8081/chatMessages?chatId=${classChatId}`, { withCredentials: true }).then((response) => {
-            console.log(response);
+            console.log(response.data)
             setContents(response.data);
+            setIsLoading(true);
         }).catch(err => {
             console.log(err);
         })
@@ -37,51 +49,68 @@ export default function ChatDisplay({ rewind, value, classChatId, chatTitle, cha
 
     useEffect(() => {
         socket.emit('joinroom', state + classChatId);
-
         socket.on('broadcast', function (data) {
-            console.log(data);
-            setContents(prevContents => [...prevContents, { content: data.content, nickName: data.nickName }]);
+            console.log(data.user)
+            setContents(prevContents => [...prevContents, { createdAt: data.createdAt, content: data.content, user: data.user }]);
         });
         getMessage();
     }, [])
 
+    useEffect(() => {
+        if (stackRef.current) {
+            stackRef.current.scrollTop = stackRef.current.scrollHeight;
+        }
+    }, [contents]);
+
     function send() {
         if (sendData !== '') {
-            socket.emit('send', { chatUserId: chatUserId, data: sendData, chatId: classChatId, chatCode: state + classChatId, nickName: user.userData.nickName });
+            socket.emit('send', { chatUserId: chatUserId, data: sendData, chatId: classChatId, chatCode: state + classChatId, userId: user.userData.id });
+            setSendData('');
         }
     }
 
     return (
         <>
-            <ArrowBackRoundedIcon sx={{ cursor: 'pointer' }} onClick={() => { rewind(); }} />
-            <div>{chatTitle}</div>
             {
-                contents.map(data => {
-                    console.log(data.content)
-                    return (
-                        <>
-                            {data.nickName === user.userData.nickName ? <RightChat content={data.content} /> : <LeftChat content={data.content} />}
-                        </>
-                    );
-                })
+                isLoading ?
+                    <>
+                        <ArrowBackRoundedIcon sx={{ cursor: 'pointer' }} onClick={() => { rewind(); }} />
+                        <Typography variant="h4">{chatTitle}</Typography>
+                        <Stack
+                            ref={stackRef}
+                            sx={{ overflowY: 'auto', height: '330px', marginBottom: `10px`, borderBottom: '1px solid black' }}>
+                            {
+                                contents.map(data => {
+                                    return (
+                                        <>
+                                            {
+                                                data.user.id === user.userData.id ?
+                                                    <RightChat content={data.content} createdAt={data.createdAt} /> :
+                                                    <LeftChat content={data.content} user={data.user} createdAt={data.createdAt} />
+                                            }
+                                        </>
+                                    );
+                                })
+                            }
+                        </Stack>
+                        <Grid container>
+                            <Grid item xs={4}>
+                                <TextField
+                                    hiddenLabel
+                                    id="filled-hidden-label-small"
+                                    placeholder="입력하세요"
+                                    variant="filled"
+                                    size="small"
+                                    onChange={(e) => { setSendData(e.target.value) }}
+                                    value={sendData}
+                                />
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Button variant="contained" color="success" onClick={send}>보내기</Button>
+                            </Grid>
+                        </Grid >
+                    </> : null
             }
-            <Grid container>
-                <Grid item xs={4}>
-                    <TextField
-                        hiddenLabel
-                        id="filled-hidden-label-small"
-                        placeholder="입력하세요"
-                        variant="filled"
-                        size="small"
-                        onChange={(e) => { setSendData(e.target.value) }}
-                        value={sendData}
-                    />
-                </Grid>
-                <Grid item xs={4}>
-                    <Button variant="contained" color="success" onClick={send}>보내기</Button>
-                </Grid>
-            </Grid>
         </>
-
     )
 }
